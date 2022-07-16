6b437c91723ba89a2814ca262f77a6fe845356e0
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2022-07-17 02:56:58 +0900

    올려주세요 진우님

---
+++ Assets/Oculus/VR/Scripts/Util/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public bool[] isTrueObjects;
+    public bool[] isFalseObjects;
+
+    public GameObject[] lights;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    public void OffLights()
+    {
+        for(int i =0; i < lights.Length; i++)
+        {
+            lights[i].SetActive(false);
+        }
+    }
+
+    
+}
--- Assets/Oculus/VR/Scripts/Util/Item.cs
+++ Assets/Oculus/VR/Scripts/Util/Item.cs
@@ -8,21 +8,46 @@ public class Item : MonoBehaviour
     public Vector3 slotRotation = Vector3.zero;
     public Slot currentSlot;
 
+    public enum itemValue { 진짜, 가짜};
+    public itemValue IV;
     public enum itemState { 기계심장, 지푸라기, 용기물약 };
-    itemState IS;
+    public itemState IS;
 
-    public void GetItem()
+    public void CheckItemValue()
     {
-        switch (IS)
+        if(IV == itemValue.가짜)
         {
-            case itemState.기계심장:
-                break;
-            case itemState.지푸라기:
-                break;
-            case itemState.용기물약:
-                break;
-            default:
-                break;
+            switch (IS)
+            {
+                case itemState.기계심장:
+                    GameManager.instance.isFalseObjects[0] = true;
+                    break;
+                case itemState.지푸라기:
+                    GameManager.instance.isFalseObjects[1] = true;
+                    break;
+                case itemState.용기물약:
+                    GameManager.instance.isFalseObjects[2] = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            switch (IS)
+            {
+                case itemState.기계심장:
+                    GameManager.instance.isTrueObjects[0] = true;
+                    break;
+                case itemState.지푸라기:
+                    GameManager.instance.isTrueObjects[1] = true;
+                    break;
+                case itemState.용기물약:
+                    GameManager.instance.isTrueObjects[2] = true;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
